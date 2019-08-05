5c7f2eb8a5a65c3fd6dfea7e077b326b872b37b2
Enping <enping.jep@alipay.com>
2019-08-05 10:11:25 +0800

    003

--- src/main/java/com/github/jep/leetcode/Medium_003_LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/github/jep/leetcode/Medium_003_LongestSubstringWithoutRepeatingCharacters.java
@@ -7,26 +7,16 @@ import java.util.Set;
 
 /**
  * @author enping
- * @version 1.0
- * 2019/8/1 22:05:22
+ * @version 1.0 2019/8/1 22:05:22
  * <p>
  * Given a string, find the length of the longest substring without repeating characters.
  * <p>
- * Example 1:
- * Input: "abcabcbb"
- * Output: 3
- * Explanation: The answer is "abc", with the length of 3.
+ * Example 1: Input: "abcabcbb" Output: 3 Explanation: The answer is "abc", with the length of 3.
  * <p>
- * Example 2:
- * Input: "bbbbb"
- * Output: 1
- * Explanation: The answer is "b", with the length of 1.
+ * Example 2: Input: "bbbbb" Output: 1 Explanation: The answer is "b", with the length of 1.
  * <p>
- * Example 3:
- * Input: "pwwkew"
- * Output: 3
- * Explanation: The answer is "wke", with the length of 3.
- * Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ * Example 3: Input: "pwwkew" Output: 3 Explanation: The answer is "wke", with the length of 3. Note
+ * that the answer must be a substring, "pwke" is a subsequence and not a substring.
  */
 
 public class Medium_003_LongestSubstringWithoutRepeatingCharacters {
@@ -35,15 +25,14 @@ public class Medium_003_LongestSubstringWithoutRepeatingCharacters {
   public static int lengthOfLongestSubstring(String s) {
     int ret = 0;
     Map<Character, Integer> map = new HashMap<Character, Integer>();
-    for (int i = 0, start = 0; i < s.length(); i++) {
-      char c = s.charAt(i);
+    for (int end = 0, start = 0; end < s.length(); end++) {
+      char c = s.charAt(end);
       if (map.containsKey(c)) {
         System.out.println("start=" + start);
         start = Math.max(map.get(c) + 1, start);
       }
-      ret = Math.max(ret, i - start + 1);
-      map.put(c, i);
-
+      ret = Math.max(ret, end - start + 1);
+      map.put(c, end);
     }
     return ret;
   }
